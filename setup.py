981ce8d28a484b2408cefa0a6ba0d21bdf5180bb
Erekle Magradze <erekle.magradze@recogizer.de>
2018-02-01 15:57:34 +0100

    adding README.rst to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='pyarchiver',
       version='0.1',
       description='Module to archive the plain files, with given (available)\
