2cc803455398ef7bae5d773c36cb1d0d2da76f15
Erekle Magradze <erekle.magradze@recogizer.de>
2018-02-01 19:53:09 +0100

    moving to solid solution.

--- pyarchiver/pyarchiver.py
+++ pyarchiver/pyarchiver.py
@@ -5,57 +5,50 @@ import os
 
 class pyarchiver:
 	""".Basic pyarchiver class."""
-    def __init__(self,
-				file_to_archive,
-				archivation_path,
-				archive_filename,
-				archiving_method
-				):
-        self.file_to_archive = file_to_archive
-        self.archivation_path = archivation_path
+	def __init__(self,file_to_archive,archivation_path,archive_filename,archiving_method):
+		self.file_to_archive = file_to_archive
+		self.archivation_path = archivation_path
 		self.archive_filename = archive_filename
 		self.archiving_method = archiving_method
 		self.archiving_methods= ['bz2','gzip','lzma']
 
 
 	def get_archivation_methods(self):
-		""" . """
+		""".Returns the all available methods for\
+		 archiving the file with this tool"""
 		return self.archiving_methods
 
-	def check_if_path_exists(full_path_to_file_or_folder):
+
+	def check_if_path_exists(self):
 		""".If file/folder exist return True, else False."""
-		the_path = full_path_to_file_or_folder
 		try:
-			if os.path.exists(the_path):
+			if os.path.exists(self.file_to_archive):
 				return True
 			else:
 				return False
 		except OSError:
-			print("Check if you have permissions to %s"%the_path)
+			print("Check if you have permissions to %s"%self.file_to_archive)
 			return False
 		return False
 
 
-	def create_path(the_path):
+	def create_path(self):
 		""".Checks if folder exists and creates if not."""
-		if os.path.exists(the_path):
+		if os.path.exists(self.archivation_path):
 			return True
 		else:
 			try:
-				os.makedirs(the_path)
+				os.makedirs(self.archivation_path)
 				return True
 			except OSError:
-				print("Check if you have permissions to %s"%the_path)
+				print("Check if you have permissions to %s"%self.archivation_path)
 				return False
 		return False
 
 
-	def pyarchiver(file_to_archive,
-				   archivation_path,
-				   archive_filename,
-				   archiving_method):
+	def pyarchiver(self):
 		""".The main function doing the archivation."""
-		if check_if_path_exists(file_to_archive):
+		if self.check_if_path_exists():
 			return True
 		else:
 			return False
--- pyarchiver/tests/test_pyarchiver.py
+++ pyarchiver/tests/test_pyarchiver.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 
-import pyarchiver
+from pyarchiver import pyarchiver
 
 
 class TestPyarchiver(TestCase):
@@ -10,13 +10,13 @@ class TestPyarchiver(TestCase):
         path_of_archive="/tmp"
         archive_file="pyarchiver"
         method='lzma'
-        s = pyarchiver.pyarchiver(file_to_archive,path_of_archive,archive_file,method)
-        self.assertTrue(s)
+        s = pyarchiver(file_to_archive,path_of_archive,archive_file,method)
+        self.assertTrue(s.pyarchiver())
 
-    def test_2(self):
-        file_to_archive="/tmp/pyarchiver_asdasdasdasd.txt"
-        path_of_archive="/tmp"
-        archive_file="pyarchiver"
-        method='lzma'
-        s = pyarchiver.pyarchiver(file_to_archive,path_of_archive,archive_file,method)
-        self.assertTrue(s)
+    # def test_2(self):
+    #     file_to_archive="/tmp/pyarchiver_asdasdasdasd.txt"
+    #     path_of_archive="/tmp"
+    #     archive_file="pyarchiver"
+    #     method='gzip'
+    #     s = pyarchiver(file_to_archive,path_of_archive,archive_file,method)
+    #     self.assertTrue(s.pyarchiver())
